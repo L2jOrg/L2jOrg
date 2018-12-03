bc56f606de305e9fb673e3b0ddeb797511bb3f2d
Alisson Oliveira <joe.alisson@gmail.com>
2018-12-03 14:09:35 -0300

    Fix Restart
    #Closes #3

--- Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/network/l2/s2c/RestartResponsePacket.java
+++ Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/network/l2/s2c/RestartResponsePacket.java
@@ -1,21 +1,26 @@
 package org.l2j.gameserver.network.l2.s2c;
 
-public class RestartResponsePacket extends L2GameServerPacket
-{
+import org.l2j.mmocore.StaticPacket;
+
+@StaticPacket
+public class RestartResponsePacket extends L2GameServerPacket  {
 	public static final RestartResponsePacket OK = new RestartResponsePacket(1), FAIL = new RestartResponsePacket(0);
 	private String _message;
 	private int _param;
 
-	public RestartResponsePacket(int param)
-	{
+	private RestartResponsePacket(int param) {
 		_message = "bye";
 		_param = param;
 	}
 
 	@Override
-	protected final void writeImpl()
-	{
+	protected final void writeImpl() {
 		writeInt(_param); //01-ok
 		writeString(_message);
+	}
+
+	@Override
+	protected int packetSize() {
+		return 7 + _message.length() * 2;
 	}
 }
