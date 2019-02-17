d82833abe99e52d573d220294ea6ee36f819bb45
Alisson Oliveira <joe.alisson@gmail.com>
2019-02-17 18:19:02 -0300

    Start gameserver using modules

--- Gameserver/bin/gameserver.sh
+++ Gameserver/bin/gameserver.sh
@@ -3,7 +3,7 @@
 err=1
 until [ $err == 0 ];
 do
-	java -Dfile.encoding=UTF-8 -Xms512m -Xmx2g -cp 'config:./lib/*' org.l2j.gameserver.GameServer
+	java -Dfile.encoding=UTF-8 -Xms512m -Xmx2g -p ./lib -cp 'config:./lib/*' -m org.l2j.gameserver/org.l2j.gameserver.GameServer
     err=$?
 	sleep 10;
 done
