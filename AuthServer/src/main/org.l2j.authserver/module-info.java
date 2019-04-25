3a52ec21c272dfcff54868eb0c8723218fa68cbf
Alisson Oliveira <joe.alisson@gmail.com>
2019-04-25 15:32:47 -0300

    remove opens to JAXB
    Signed-off-by: Alisson Oliveira <joe.alisson@gmail.com>

--- AuthServer/src/main/org.l2j.authserver/module-info.java
+++ AuthServer/src/main/org.l2j.authserver/module-info.java
@@ -6,7 +6,6 @@ module org.l2j.authserver {
     requires java.xml;
 
     exports org.l2j.authserver;
-    opens org.l2j.authserver.data.xml to java.xml.bind;
     opens org.l2j.authserver.settings to org.l2j.commons;
     opens org.l2j.authserver.data.database to org.l2j.commons;
 }
