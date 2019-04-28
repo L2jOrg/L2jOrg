587c560ef09173edb924d09cfaf974ab6597ced0
Alisson Oliveira <joe.alisson@gmail.com>
2019-04-28 09:32:26 -0300

    Fix Move Shortcuts

--- Datapack/gameserver/sql/install/character_shortcuts.sql
+++ Datapack/gameserver/sql/install/character_shortcuts.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS `character_shortcuts`;
-CREATE TABLE IF NOT EXISTS `character_shortcuts` (
+CREATE TABLE `character_shortcuts` (
   `charId` INT UNSIGNED NOT NULL DEFAULT 0,
   `slot` decimal(3) NOT NULL DEFAULT 0,
   `page` decimal(3) NOT NULL DEFAULT 0,
   `type` decimal(3) ,
   `shortcut_id` decimal(16) ,
-  `level` SMALLINT UNSIGNED ,
+  `level` SMALLINT,
   `sub_level` INT(4) NOT NULL DEFAULT '0',
   `class_index` int(1) NOT NULL DEFAULT '0',
 
