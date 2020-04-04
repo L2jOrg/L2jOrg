323a2c7350dd27084cacc833ef37ef63b319f01f
vicochips <victor.fraysse@epitech.eu>
2020-04-04 18:08:22 +0200

    fix packet

--- Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/network/clientpackets/ExOpenHtml.java
+++ Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/network/clientpackets/ExOpenHtml.java
@@ -1,6 +1,7 @@
 package org.l2j.gameserver.network.clientpackets;
 
 import org.l2j.gameserver.cache.HtmCache;
+import org.l2j.gameserver.network.serverpackets.ExPurchaseLimitShopItemList;
 import org.l2j.gameserver.network.serverpackets.html.ExPremiumManagerShowHtml;
 
 /**
@@ -9,15 +10,19 @@ import org.l2j.gameserver.network.serverpackets.html.ExPremiumManagerShowHtml;
 public class ExOpenHtml extends ClientPacket {
 
     private static final String COMMON_HTML_PATH = "data/html/common/%d.htm";
-    private byte html;
+    private byte dialogId;
 
     @Override
     protected void readImpl() throws Exception {
-        html = readByte();
+        dialogId = readByte();
     }
 
     @Override
     protected void runImpl(){
-        client.sendPacket( new ExPremiumManagerShowHtml( HtmCache.getInstance().getHtm( client.getPlayer(), String.format(COMMON_HTML_PATH, html ))));
+        switch (dialogId) {
+            // case 1 -> pc points
+            case 4 -> client.sendPacket(new ExPurchaseLimitShopItemList());
+            case 5 -> client.sendPacket( new ExPremiumManagerShowHtml( HtmCache.getInstance().getHtm( client.getPlayer(), String.format(COMMON_HTML_PATH, dialogId))));
+        }
     }
 }
