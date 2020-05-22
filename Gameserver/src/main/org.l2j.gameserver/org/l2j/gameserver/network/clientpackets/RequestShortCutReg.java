583a363c9d5ef7d222916497fb7c050659c1d9ec
Alisson Oliveira <joe.alisson@gmail.com>
2020-05-21 21:24:06 -0300

    Auto potions are activated when registered on shortcut
    Signed-off-by: Alisson Oliveira <joe.alisson@gmail.com>
    
    #159

--- Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/network/clientpackets/RequestShortCutReg.java
+++ Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/network/clientpackets/RequestShortCutReg.java
@@ -1,8 +1,10 @@
 package org.l2j.gameserver.network.clientpackets;
 
 import org.l2j.gameserver.data.database.data.Shortcut;
+import org.l2j.gameserver.engine.autoplay.AutoPlayEngine;
 import org.l2j.gameserver.enums.ShortcutType;
 import org.l2j.gameserver.model.item.instance.Item;
+import org.l2j.gameserver.network.serverpackets.autoplay.ExActivateAutoShortcut;
 
 import static java.util.Objects.isNull;
 
@@ -37,12 +39,18 @@ public final class RequestShortCutReg extends ClientPacket {
         }
 
         var player = client.getPlayer();
+        Item item = null;
+        if(type == ShortcutType.ITEM && isNull(item = player.getInventory().getItemByObjectId(id))) {
+            return;
+        }
 
-        Item item;
-        if(room == Shortcut.AUTO_POTION_ROOM && (type != ShortcutType.ITEM || isNull(item = player.getInventory().getItemByObjectId(id)) || !item.isAutoPotion())) {
+        if(room == Shortcut.AUTO_POTION_ROOM && (isNull(item) || !item.isAutoPotion())) {
             return;
         }
 
         player.registerShortCut(new Shortcut(room, type, id, lvl, subLvl, characterType));
+        if(room == Shortcut.AUTO_POTION_ROOM && AutoPlayEngine.getInstance().setActiveAutoShortcut(player, room, true)) {
+            client.sendPacket(new ExActivateAutoShortcut(room, true));
+        }
     }
 }
