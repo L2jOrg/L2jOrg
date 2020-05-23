535c8f444d66e0eaaceb6d907b9596dcc51ecb76
Thossine <christophelopes.free@free.fr>
2020-05-23 20:18:00 +0200

    Add skills implementation for Craft
    Implementing passve skills:
     - CREATE MASTER
     - CREATE CRITICAL

--- Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/RecipeController.java
+++ Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/RecipeController.java
@@ -320,7 +320,7 @@ public class RecipeController {
             if (_items == null) {
                 // handle possible cheaters here
                 // (they click craft then try to get rid of items in order to get free craft)
-            } else if (Rnd.get(100) < _recipeList.getSuccessRate()) { // TODO: modify depending on skill level
+            } else if (Rnd.get(100) < _recipeList.getSuccessRate() + getChanceRateModifier()) { // TODO: How Modifier +10 % should affect the successRate ? successRate + Modifier (60% + 10% = 70%) or successRate * Modifier (60% * 110% = 66%) (@Pearlbear)
                 rewardPlayer(); // and immediately puts created item in its place
                 updateMakeInfo(true);
             } else {
@@ -346,6 +346,44 @@ public class RecipeController {
             _activeMakers.remove(_player.getObjectId());
             _player.setIsCrafting(false);
             _target.sendItemList();
+        }
+
+        private float getChanceRateModifier() {
+            // TODO: move modifier to skill template (@Thoss, @JoeAlisson)
+            int skillLevel = _player.getSkillLevel(CommonSkill.CREATE_MASTER.getId());
+
+            switch (skillLevel) {
+                case 1:
+                    return 5;
+
+                case 2:
+                    return 10;
+
+                case 3:
+                    return 20;
+
+                default:
+                    return 0;
+            }
+        }
+
+        private float getChanceCriticalModifier() {
+            // TODO: move modifier to skill template (@Thoss, @JoeAlisson)
+            int skillLevel = _player.getSkillLevel(CommonSkill.CREATE_CRITICAL.getId());
+
+            switch (skillLevel) {
+                case 1:
+                    return 3;
+
+                case 2:
+                    return 5;
+
+                case 3:
+                    return 10;
+
+                default:
+                    return 0;
+            }
         }
 
         private void updateMakeInfo(boolean success) {
@@ -516,7 +554,14 @@ public class RecipeController {
             final int rareProdId = _recipeList.getRareItemId();
             int itemId = _recipeList.getItemId();
             int itemCount = _recipeList.getCount();
-            final ItemTemplate template = ItemEngine.getInstance().getTemplate(itemId);
+
+            // TODO: Find default value for Critical Craft (@Pearlbear)
+            // TODO: This test should be moved after code below if applicable on rare production items (@Pearlbear)
+            // TODO: How Modifier +10 % should affect the successRate ? successRate + Modifier (5% + 10% = 15%) or successRate * Modifier (5% * 110% = 5.5%) (@Pearlbear)
+            if (Rnd.get(100) < 5 + getChanceCriticalModifier()) {
+                itemCount *= 2;
+                // TODO: Send message for critical craft or not ? (@Pearlbear)
+            }
 
             // check that the current recipe has a rare production or not
             if ((rareProdId != -1) && ((rareProdId == itemId) || Config.CRAFT_MASTERWORK)) {
