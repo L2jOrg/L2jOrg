da26dfa951f69a192855e1278d92ee53eb113dad
Thossine <christophelopes.free@free.fr>
2020-06-14 17:47:08 +0200

    Update Mirrors.java

--- Datapack/gameserver/data/scripts/org.l2j.scripts/ai/areas/ForestOfTheMirrors/Mirrors.java
+++ Datapack/gameserver/data/scripts/org.l2j.scripts/ai/areas/ForestOfTheMirrors/Mirrors.java
@@ -14,18 +14,18 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * @author Thoss
  */
-// FIXME: Dev in process
 public class Mirrors extends AbstractNpcAI {
     private static final Logger LOGGER = LoggerFactory.getLogger(Mirrors.class);
 
-    private final int MIRROR_NPC_ID = 20639;
-    private final int DESPAWN_TIME = 600000;
-    private final int MIRROR_COUNT = 4;
+    private static final int MIRROR_NPC_ID = 20639;
+    private static final int DESPAWN_TIME = 600000;
+    private static final int MIRROR_COUNT = 4;
 
-    private Map<Integer, Integer> _Leaders_Stages = new ConcurrentHashMap<>(); // <Leader ObjectID, Leader Stage>
-    private Map<Integer, List<Integer>> _Leader_Minions = new ConcurrentHashMap<>(); // <Leader ObjectID, Leader minions ObjectID>
+    private static Map<Integer, Integer> _Leaders_Stages = new ConcurrentHashMap<>(); // <Leader ObjectID, Leader Stage>
+    private static Map<Integer, List<Integer>> _Leader_Minions = new ConcurrentHashMap<>(); // <Leader ObjectID, Leader minions ObjectID>
+    private static Map<Integer, Boolean> _Minions_State = new ConcurrentHashMap<>(); // <Minion ObjectID, isAlive>
 
-    private Mirrors()
+    public Mirrors()
     {
         addKillId(MIRROR_NPC_ID);
     }
@@ -37,28 +37,30 @@ public class Mirrors extends AbstractNpcAI {
 
             if(leaderObjectId == -1) {
                 leaderObjectId = npc.getObjectId();
-                _Leaders_Stages.putIfAbsent(leaderObjectId, 0);
-                _Leader_Minions.putIfAbsent(leaderObjectId, new ArrayList<>());
-            } else {
-                _Leader_Minions.get(leaderObjectId).add(npc.getObjectId());
-            }
-
-            switch (_Leaders_Stages.get(leaderObjectId))
-            {
-                case 0, 1, 2, 3: {
-                    if(_Leader_Minions.get(leaderObjectId).size() % 4 == 0) {
+                _Leaders_Stages.put(leaderObjectId, 0);
+                _Leader_Minions.put(leaderObjectId, new ArrayList<>());
+            } else
+                _Minions_State.put(npc.getObjectId(), false);
+            
+            if(getAliveMinions(leaderObjectId) % MIRROR_COUNT == 0)
+                switch (_Leaders_Stages.get(leaderObjectId)) {
+                    case 0, 1, 2, 3 -> {
                         for (int i = 0; i < MIRROR_COUNT; i++) {
                             Npc mirror = addSpawn(MIRROR_NPC_ID, npc, true, DESPAWN_TIME);
+                            _Leader_Minions.get(leaderObjectId).add(mirror.getObjectId());
+                            _Minions_State.put(mirror.getObjectId(), true);
                             addAttackPlayerDesire(mirror, killer);
                         }
                         _Leaders_Stages.replace(leaderObjectId, _Leaders_Stages.get(leaderObjectId) + 1);
+
+                    }
+                    case 4 -> {
+                        for (int minion : _Leader_Minions.get(leaderObjectId))
+                            _Minions_State.remove(minion);
+                        _Leader_Minions.remove(leaderObjectId);
+                        _Leaders_Stages.remove(leaderObjectId);
                     }
                 }
-                case 4: {
-                    _Leaders_Stages.remove(leaderObjectId);
-                    _Leader_Minions.remove(leaderObjectId);
-                }
-            }
         }
         return super.onKill(npc, killer, isSummon);
     }
@@ -69,6 +71,16 @@ public class Mirrors extends AbstractNpcAI {
                 return leader.getKey();
 
         return -1;
+    }
+
+    private int getAliveMinions(int leaderObjectId) {
+        int count = 0;
+
+        for(int minion : _Leader_Minions.get(leaderObjectId))
+            if(_Minions_State.get(minion) == true)
+                count++;
+
+        return count;
     }
 
     public static AbstractNpcAI provider()
