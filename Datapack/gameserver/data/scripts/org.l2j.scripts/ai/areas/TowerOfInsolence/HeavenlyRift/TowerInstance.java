8eaa52d195e2d16c26f3633aa7905e44194e387d
Thossine <christophelopes.free@free.fr>
2020-06-16 17:58:46 +0200

    Update TowerInstance.java

--- Datapack/gameserver/data/scripts/org.l2j.scripts/ai/areas/TowerOfInsolence/HeavenlyRift/TowerInstance.java
+++ Datapack/gameserver/data/scripts/org.l2j.scripts/ai/areas/TowerOfInsolence/HeavenlyRift/TowerInstance.java
@@ -10,69 +10,59 @@ import java.util.StringTokenizer;
 /**
  * @reworked by Thoss
  */
-public class TowerInstance extends Folk
-{
-	private static int[] ITEM_REWARD = { 49764, 49765 };
-	
+public class TowerInstance extends Folk {
+	private static int[] ITEM_REWARD = {49764, 49765};
+
 	public TowerInstance(NpcTemplate template) {
 		super(template);
 	}
 
 	@Override
-	public void onBypassFeedback(Player player, String command)
-	{
+	public void onBypassFeedback(Player player, String command) {
 		StringTokenizer st = new StringTokenizer(command, "_");
 		String cmd = st.nextToken();
-		if(cmd.equals("getreward"))
-		{
-			if(GlobalVariablesManager.getInstance().getInt("heavenly_rift_reward", 0) == 1)
-			{
+		if (cmd.equals("getreward")) {
+			if (GlobalVariablesManager.getInstance().getInt("heavenly_rift_reward", 0) == 1) {
 				GlobalVariablesManager.getInstance().set("heavenly_rift_reward", 0);
-				if(player.isGM())
-				{
-					for(int r : ITEM_REWARD)
+				if (player.isGM()) {
+					for (int r : ITEM_REWARD)
 						player.addItem("TowerInstance", r, 1, this, true);
-				}
-				else
-				{
-					for(Player partyMember : player.getParty().getMembers())
-					{
-						for(int r : ITEM_REWARD)
+				} else {
+					for (Player partyMember : player.getParty().getMembers()) {
+						for (int r : ITEM_REWARD)
 							partyMember.addItem("TowerInstance", r, 1, this, true);
 					}
-				}	
+				}
 				showChatWindow(player, "default/" + getId() + "-3.htm");
-			}	
-		}	
-		else
+			}
+		} else
 			super.onBypassFeedback(player, command);
 	}
 
 	@Override
 	public String getHtmlPath(int npcId, int val) {
-	{
-		String filename = "";
-
-		if(!getActingPlayer().isGM() && (!getActingPlayer().isInParty() || !getActingPlayer().getParty().isLeader(getActingPlayer())))
-			filename = getId() + "-4.htm";
-		else if(!isDead() && GlobalVariablesManager.getInstance().getInt("heavenly_rift_complete", 0) == 2)
 		{
-			if(GlobalVariablesManager.getInstance().getInt("heavenly_rift_reward", 0) == 1)
-				filename = getId() + ".htm";
-			else
-				filename = getId() + "-2.htm";
+			String filename = "";
+
+			if (!getActingPlayer().isGM() && (!getActingPlayer().isInParty() || !getActingPlayer().getParty().isLeader(getActingPlayer())))
+				filename = getId() + "-4.htm";
+			else if (!isDead() && GlobalVariablesManager.getInstance().getInt("heavenly_rift_complete", 0) == 2) {
+				if (GlobalVariablesManager.getInstance().getInt("heavenly_rift_reward", 0) == 1)
+					filename = getId() + ".htm";
+				else
+					filename = getId() + "-2.htm";
+			} else
+				filename = getId() + "-1.htm";
+
+			return filename;
 		}
-		else
-			filename = getId() + "-1.htm";
 
-		return filename;
-	}
-
-	// TODO: think about that ...
+		// TODO: think about that ...
 	/*
 	@Override
 	public boolean isDebuffImmune()
 	{
 		return false;
 	}*/
+	}
 }
