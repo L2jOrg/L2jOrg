a6a5e69fb94a5aeeab0823d53f4aaff629913d1e
Alisson Oliveira <joe.alisson@gmail.com>
2020-11-05 18:01:42 -0300

    Fix remove exp without delevel
    FIX JoeAlisson/L2jOrg#465

--- Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/model/actor/stat/PlayableStats.java
+++ Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/model/actor/stat/PlayableStats.java
@@ -104,6 +104,10 @@ public class PlayableStats extends CreatureStats {
             value = getExp() - 1;
         }
 
+        if(value == 0) {
+            return false;
+        }
+
         setExp(getExp() - value);
 
         byte minimumLevel = 1;
@@ -113,7 +117,7 @@ public class PlayableStats extends CreatureStats {
         }
         byte level = minimumLevel;
 
-        for (byte tmp = level; tmp <= getMaxLevel(); tmp++) {
+        for (byte tmp = level; tmp <= getMaxLevel() + 1; tmp++) {
             if (getExp() >= getExpForLevel(tmp)) {
                 continue;
             }
