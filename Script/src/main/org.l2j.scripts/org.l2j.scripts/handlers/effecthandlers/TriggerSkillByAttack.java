674b27968e0a307eae46b86aaf4935f7c45a8cf0
Alisson Oliveira <joe.alisson@gmail.com>
2020-12-02 20:58:49 -0300

    FIX Trigger Skill by Attack effect
    FIX JoeAlisson/L2jOrg#470

--- Script/src/main/org.l2j.scripts/org.l2j.scripts/handlers/effecthandlers/TriggerSkillByAttack.java
+++ Script/src/main/org.l2j.scripts/org.l2j.scripts/handlers/effecthandlers/TriggerSkillByAttack.java
@@ -19,10 +19,8 @@
 package org.l2j.scripts.handlers.effecthandlers;
 
 import org.l2j.commons.util.Rnd;
-import org.l2j.commons.util.Util;
 import org.l2j.gameserver.engine.skill.api.Skill;
 import org.l2j.gameserver.engine.skill.api.SkillEffectFactory;
-import org.l2j.gameserver.enums.InstanceType;
 import org.l2j.gameserver.handler.TargetHandler;
 import org.l2j.gameserver.model.StatsSet;
 import org.l2j.gameserver.model.WorldObject;
@@ -38,8 +36,10 @@ import org.l2j.gameserver.model.skills.BuffInfo;
 import org.l2j.gameserver.model.skills.SkillCaster;
 import org.l2j.gameserver.model.skills.targets.TargetType;
 
+import java.util.EnumSet;
 import java.util.function.Consumer;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
 import static org.l2j.gameserver.util.GameUtils.isCreature;
 
@@ -49,52 +49,45 @@ import static org.l2j.gameserver.util.GameUtils.isCreature;
  * @author JoeAlisson
  */
 public final class TriggerSkillByAttack extends AbstractEffect {
+
     private final int minDamage;
     private final int chance;
     private final SkillHolder skill;
     private final TargetType targetType;
-    private final InstanceType instanceType;
-    private int allowWeapons;
-    private final Boolean isCritical;
-    private final boolean allowNormalAttack;
-    private final boolean allowSkillAttack;
+    private final EnumSet<WeaponType> weaponsAllowed;
+
+    private final boolean criticalOnly;
+    private final boolean triggerByNormalAttack;
+    private final boolean triggerBySkill;
     private final boolean allowReflect;
 
     private TriggerSkillByAttack(StatsSet params) {
-        minDamage = params.getInt("min-damage", 1);
+        minDamage = params.getInt("min-damage", 0);
         chance = params.getInt("chance", 100);
         skill = new SkillHolder(params.getInt("skill"), params.getInt("power", 1));
         targetType = params.getEnum("target", TargetType.class, TargetType.SELF);
-        instanceType = params.getEnum("attackerType", InstanceType.class, InstanceType.Creature);
-        isCritical = params.getBoolean("critical");
-        allowNormalAttack = params.getBoolean("allowNormalAttack", true);
-        allowSkillAttack = params.getBoolean("allowSkillAttack", false);
-        allowReflect = params.getBoolean("allowReflect", false);
 
-        if(params.contains("weapons") && Util.isNotEmpty(params.getString("weapons"))) {
-            for (String s : params.getString("weapons").split(" ")) {
-                allowWeapons |= WeaponType.valueOf(s).mask();
-            }
-        }
+        criticalOnly = params.getBoolean("critical-only");
+        triggerByNormalAttack = params.getBoolean("trigger-by-normal-attack");
+        triggerBySkill = params.getBoolean("trigger-by-skill");
+        allowReflect = params.getBoolean("allow-reflect", false);
+        weaponsAllowed = params.getStringAsEnumSet("weapons", WeaponType.class);
     }
 
     private void onAttackEvent(OnCreatureDamageDealt event) {
-        if (event.isDamageOverTime() || (chance == 0) || ((skill.getSkillId() == 0) || (skill.getLevel() == 0)) || (!allowNormalAttack && !allowSkillAttack)) {
+        if (event.isDamageOverTime() || (chance == 0) || ((skill.getSkillId() == 0) || (skill.getLevel() == 0)) || (!triggerByNormalAttack && !triggerBySkill)) {
             return;
         }
 
-        // Check if there is dependancy on critical.
-        if (nonNull(isCritical) && (isCritical != event.isCritical())) {
+        if (criticalOnly && !event.isCritical()) {
             return;
         }
 
-        // When no skill attacks are allowed.
-        if (!allowSkillAttack && nonNull(event.getSkill())) {
+        if (!triggerBySkill && nonNull(event.getSkill())) {
             return;
         }
 
-        // When no normal attacks are allowed.
-        if (!allowNormalAttack && nonNull(event.getSkill())) {
+        if (!triggerByNormalAttack && isNull(event.getSkill())) {
             return;
         }
 
@@ -106,14 +99,13 @@ public final class TriggerSkillByAttack extends AbstractEffect {
             return;
         }
 
-        if (event.getDamage() < minDamage || !Rnd.chance(chance) || !event.getAttacker().getInstanceType().isType(instanceType)) {
+        if (event.getDamage() < minDamage || !Rnd.chance(chance)) {
             return;
         }
 
-        if (allowWeapons > 0) {
-            if ((event.getAttacker().getActiveWeaponItem() == null) || ((event.getAttacker().getActiveWeaponItem().getItemType().mask() & allowWeapons) == 0)) {
-                return;
-            }
+        var weapon = event.getAttacker().getActiveWeaponItem();
+        if(!weaponsAllowed.isEmpty() && (isNull(weapon) || !weaponsAllowed.contains(weapon.getItemType()))) {
+            return;
         }
 
         final Skill triggerSkill = skill.getSkill();
@@ -121,13 +113,12 @@ public final class TriggerSkillByAttack extends AbstractEffect {
         try {
             target = TargetHandler.getInstance().getHandler(targetType).getTarget(event.getAttacker(), event.getTarget(), triggerSkill, false, false, false);
         } catch (Exception e) {
-            LOGGER.warn("Exception in ITargetTypeHandler.getTarget(): " + e.getMessage(), e);
+            LOGGER.warn("Exception in ITargetTypeHandler.getTarget(): {}", e.getMessage(), e);
         }
 
-        if (isCreature(target))
-        {
+        if (isCreature(target)) {
             final BuffInfo info = ((Creature) target).getEffectList().getBuffInfoBySkillId(triggerSkill.getId());
-            if ((info == null) || (info.getSkill().getLevel() < triggerSkill.getLevel()))
+            if (isNull(info) || (info.getSkill().getLevel() < triggerSkill.getLevel()))
             {
                 SkillCaster.triggerCast(event.getAttacker(), (Creature) target, triggerSkill);
             }
@@ -141,8 +132,7 @@ public final class TriggerSkillByAttack extends AbstractEffect {
     }
 
     @Override
-    public void onStart(Creature effector, Creature effected, Skill skill, Item item)
-    {
+    public void onStart(Creature effector, Creature effected, Skill skill, Item item) {
         effected.addListener(new ConsumerEventListener(effected, EventType.ON_CREATURE_DAMAGE_DEALT, (Consumer<OnCreatureDamageDealt>) this::onAttackEvent, this));
     }
 
