bbf00990de9cab455cb72ed003aa6ca82e6911f9
Alisson Oliveira <joe.alisson@gmail.com>
2021-03-16 19:58:30 -0300

    Remove dead methods

--- Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/data/database/data/PlayerVariableData.java
+++ Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/data/database/data/PlayerVariableData.java
@@ -51,20 +51,11 @@ public class PlayerVariableData {
     @Column("ability_points_main_class_used")
     private int abilityPointsMainClassUsed;
 
-    @Column("ability_points_dual_class_used")
-    private int abilityPointsDualClassUsed;
-
     @Column("revelation_skill_main_class_1")
     private int revelationSkillMainClass1;
 
     @Column("revelation_skill_main_class_2")
     private int revelationSkillMainClass2;
-
-    @Column("revelation_skill_dual_class_1")
-    private int revelationSkillDualClass1;
-
-    @Column("revelation_skill_dual_class_2")
-    private int revelationSkillDualClass2;
 
     @Column("extend_drop")
     private String extendDrop;
@@ -120,12 +111,6 @@ public class PlayerVariableData {
     @Column("ui_key_mapping")
     private String uiKeyMapping;
 
-    @Column("attendance_date")
-    private long attendanceDate;
-
-    @Column("attendance_index")
-    private int attendanceIndex;
-
     @Column("monster_return")
     private int monsterReturn;
 
@@ -154,24 +139,12 @@ public class PlayerVariableData {
         return abilityPointsMainClassUsed;
     }
 
-    public int getAbilityPointsDualClassUsed() {
-        return abilityPointsDualClassUsed;
-    }
-
     public int getRevelationSkillMainClass1() {
         return revelationSkillMainClass1;
     }
 
     public int getRevelationSkillMainClass2() {
         return revelationSkillMainClass2;
-    }
-
-    public int getRevelationSkillDualClass1() {
-        return revelationSkillDualClass1;
-    }
-
-    public int getRevelationSkillDualClass2() {
-        return revelationSkillDualClass2;
     }
 
     public String getExtendDrop() {
@@ -250,14 +223,6 @@ public class PlayerVariableData {
         return uiKeyMapping;
     }
 
-    public long getAttendanceDate() {
-        return attendanceDate;
-    }
-
-    public int getAttendanceIndex() {
-        return attendanceIndex;
-    }
-
     public int getMonsterReturn() {
         return monsterReturn;
     }
@@ -268,30 +233,6 @@ public class PlayerVariableData {
 
     public void setWorldChatUsed(int worldChatUsed) {
         this.worldChatUsed = worldChatUsed;
-    }
-
-    public void setVitalityItemsUsed(int vitalityItemsUsed) {
-        this.vitalityItemsUsed = vitalityItemsUsed;
-    }
-
-    public void setAbilityPointsMainClassUsed(int abilityPointsMainClassUsed) {
-        this.abilityPointsMainClassUsed = abilityPointsMainClassUsed;
-    }
-
-    public void setAbilityPointsDualClassUsed(int abilityPointsDualClassUsed) {
-        this.abilityPointsDualClassUsed = abilityPointsDualClassUsed;
-    }
-
-    public void setRevelationSkillMainClass1(int revelationSkillMainClass1) {
-        this.revelationSkillMainClass1 = revelationSkillMainClass1;
-    }
-
-    public void setRevelationSkillMainClass2(int revelationSkillMainClass2) {
-        this.revelationSkillMainClass2 = revelationSkillMainClass2;
-    }
-
-    public void setRevelationSkillDualClass1(int revelationSkillDualClass1) {
-        this.revelationSkillDualClass1 = revelationSkillDualClass1;
     }
 
     public void setExtendDrop(String extendDrop) {
@@ -364,14 +305,6 @@ public class PlayerVariableData {
 
     public void setCondOverrideKey(String condOverrideKey) {
         this.condOverrideKey = condOverrideKey;
-    }
-
-    public void setAttendanceDate(long attendanceDate) {
-        this.attendanceDate = attendanceDate;
-    }
-
-    public void setAttendanceIndex(int attendanceIndex) {
-        this.attendanceIndex = attendanceIndex;
     }
 
     public void setMonsterReturn(int monsterReturn) {
