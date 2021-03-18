3fa452ad923f58764eded411c256efe2049cf0fa
Alisson Oliveira <joe.alisson@gmail.com>
2021-03-18 00:32:22 -0300

    Fix soulshot additional effect

--- Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/model/item/container/Inventory.java
+++ Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/model/item/container/Inventory.java
@@ -289,7 +289,7 @@ public abstract class Inventory extends ItemContainer {
                 old.setLastChange(ItemChangeType.MODIFIED);
 
                 wearedMask &= ~old.getItemMask();
-                listeners.forEach(l -> l.notifyUnequiped(slot, old, this));
+                listeners.forEach(l -> l.notifyUnequipped(slot, old, this));
                 old.updateDatabase();
             }
 
@@ -298,7 +298,7 @@ public abstract class Inventory extends ItemContainer {
                 item.changeItemLocation(getEquipLocation(), slot.getId());
                 item.setLastChange(ItemChangeType.MODIFIED);
                 wearedMask |= item.getItemMask();
-                listeners.forEach(l -> l.notifyEquiped(slot, item, this));
+                listeners.forEach(l -> l.notifyEquipped(slot, item, this));
                 item.updateDatabase();
             }
 
@@ -694,8 +694,8 @@ public abstract class Inventory extends ItemContainer {
      */
     public void reloadEquippedItems() {
         paperdoll.forEach((slot, item) -> listeners.forEach(l -> {
-            l.notifyUnequiped(slot, item, this);
-            l.notifyEquiped(slot, item, this);
+            l.notifyUnequipped(slot, item, this);
+            l.notifyEquipped(slot, item, this);
         }));
         if (isPlayer(getOwner())) {
             getOwner().sendPacket(new ExUserInfoEquipSlot(getOwner().getActingPlayer()));
@@ -706,8 +706,8 @@ public abstract class Inventory extends ItemContainer {
         paperdoll.entrySet().stream()
             .filter(entry -> entry.getValue()== item).findFirst()
             .ifPresent(e -> listeners.forEach(l -> {
-                l.notifyUnequiped(e.getKey(), e.getValue(), this);
-                l.notifyEquiped(e.getKey(), e.getValue(), this);
+                l.notifyUnequipped(e.getKey(), e.getValue(), this);
+                l.notifyEquipped(e.getKey(), e.getValue(), this);
             }));
     }
 
@@ -788,12 +788,12 @@ public abstract class Inventory extends ItemContainer {
         }
 
         @Override
-        public void notifyEquiped(InventorySlot slot, Item item, Inventory inventory) {
+        public void notifyEquipped(InventorySlot slot, Item item, Inventory inventory) {
             changed.add(item);
         }
 
         @Override
-        public void notifyUnequiped(InventorySlot slot, Item item, Inventory inventory) {
+        public void notifyUnequipped(InventorySlot slot, Item item, Inventory inventory) {
             changed.add(item);
         }
 
