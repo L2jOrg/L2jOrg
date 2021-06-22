4f446f0d98c16dbfee85d7d7a6393a2d963b6c97
Alisson Oliveira <joe.alisson@gmail.com>
2021-06-22 09:02:30 -0300

    Temp fix database install until #39 is finished

--- Gameserver/src/main/resources/sql/install/1_characters.sql
+++ Gameserver/src/main/resources/sql/install/1_characters.sql
@@ -60,6 +60,16 @@ CREATE TABLE IF NOT EXISTS `characters`(
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8MB4;
 
+CREATE TABLE IF NOT EXISTS `clan_members`
+(
+    `clan_id`               INT          NOT NULL,
+    `player_id`             INT UNSIGNED NOT NULL,
+    `last_reputation_level` SMALLINT     NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (`clan_id`, `player_id`),
+    FOREIGN KEY (`clan_id`) REFERENCES clan_data (clan_id) ON DELETE CASCADE,
+    FOREIGN KEY (`player_id`) REFERENCES characters (charId) ON DELETE CASCADE
+);
 
 CREATE TABLE IF NOT EXISTS `rankers_snapshot`
 (
