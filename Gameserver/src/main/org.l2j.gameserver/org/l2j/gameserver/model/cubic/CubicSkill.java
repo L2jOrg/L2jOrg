125439e2fdc7a616abc25f8067303043dc074b7c
Alisson Oliveira <joe.alisson@gmail.com>
2021-07-20 07:51:06 -0300

    Refactoring Cubic skill

--- Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/model/cubic/CubicSkill.java
+++ Gameserver/src/main/org.l2j.gameserver/org/l2j/gameserver/model/cubic/CubicSkill.java
@@ -18,13 +18,11 @@
  */
 package org.l2j.gameserver.model.cubic;
 
-import org.l2j.gameserver.model.StatsSet;
+import org.l2j.gameserver.engine.skill.api.Skill;
 import org.l2j.gameserver.model.WorldObject;
 import org.l2j.gameserver.model.actor.Creature;
 import org.l2j.gameserver.model.cubic.conditions.ICubicCondition;
-import org.l2j.gameserver.model.holders.SkillHolder;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.l2j.gameserver.util.GameUtils.isCreature;
@@ -32,51 +30,17 @@ import static org.l2j.gameserver.util.GameUtils.isCreature;
 /**
  * @author UnAfraid
  */
-public class CubicSkill extends SkillHolder implements ICubicConditionHolder {
-    private final int _triggerRate;
-    private final int _successRate;
-    private final boolean _canUseOnStaticObjects;
-    private final CubicTargetType _targetType;
-    private final List<ICubicCondition> _conditions = new ArrayList<>();
-    private final boolean _targetDebuff;
-
-    public CubicSkill(StatsSet set) {
-        super(set.getInt("id"), set.getInt("level"));
-        _triggerRate = set.getInt("triggerRate", 100);
-        _successRate = set.getInt("successRate", 100);
-        _canUseOnStaticObjects = set.getBoolean("canUseOnStaticObjects", false);
-        _targetType = set.getEnum("target", CubicTargetType.class, CubicTargetType.TARGET);
-        _targetDebuff = set.getBoolean("targetDebuff", false);
-    }
-
-    public int getTriggerRate() {
-        return _triggerRate;
-    }
-
-    public int getSuccessRate() {
-        return _successRate;
-    }
-
-    public boolean canUseOnStaticObjects() {
-        return _canUseOnStaticObjects;
-    }
-
-    public CubicTargetType getTargetType() {
-        return _targetType;
-    }
+public record CubicSkill(
+     Skill skill,
+     int triggerRate,
+     int successRate,
+     boolean canUseOnStaticObjects,
+     CubicTargetType targetType,
+     boolean targetDebuff,
+     List<ICubicCondition> conditions) implements ICubicConditionHolder {
 
     @Override
     public boolean validateConditions(CubicInstance cubic, Creature owner, WorldObject target) {
-        return (!_targetDebuff || isCreature(target) && ((Creature) target).getEffectList().getDebuffCount() > 0) && (_conditions.isEmpty() || _conditions.stream().allMatch(condition -> condition.test(cubic, owner, target)));
-    }
-
-    @Override
-    public void addCondition(ICubicCondition condition) {
-        _conditions.add(condition);
-    }
-
-    @Override
-    public String toString() {
-        return "Cubic skill id: " + getSkillId() + " level: " + getLevel() + " triggerRate: " + _triggerRate + " successRate: " + _successRate + " canUseOnStaticObjects: " + _canUseOnStaticObjects + " targetType: " + _targetType + " isTargetingDebuff: " + _targetDebuff + System.lineSeparator();
+        return (!targetDebuff || isCreature(target) && ((Creature) target).getEffectList().getDebuffCount() > 0) && (conditions.isEmpty() || conditions.stream().allMatch(condition -> condition.test(cubic, owner, target)));
     }
 }
