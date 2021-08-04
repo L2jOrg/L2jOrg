860a780818ad5b882102eb3c309886b8f8c69e29
Alisson Oliveira <joe.alisson@gmail.com>
2021-08-03 22:52:28 -0300

    FIX FC's complexity issues

--- Script/src/main/org.l2j.scripts/org/l2j/scripts/handlers/admincommandhandlers/AdminPForge.java
+++ Script/src/main/org.l2j.scripts/org/l2j/scripts/handlers/admincommandhandlers/AdminPForge.java
@@ -30,8 +30,7 @@ import org.l2j.gameserver.util.BuilderUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.math.BigInteger;
-import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
@@ -77,109 +76,54 @@ public final class AdminPForge implements IAdminCommandHandler
 			return null;
 		}
 		
-		return opCodes.toArray(new String[opCodes.size()]);
+		return opCodes.toArray(new String[0]);
 	}
 	
-	private boolean validateOpCodes(String[] opCodes)
-	{
-		if ((opCodes == null) || (opCodes.length == 0) || (opCodes.length > 3))
-		{
-			return false;
+	private boolean isInvalidOpcodes(String[] opCodes) {
+		if (opCodes == null || opCodes.length == 0 || opCodes.length > 3) {
+			return true;
 		}
 		
-		for (int i = 0; i < opCodes.length; ++i)
-		{
+		for (int i = 0; i < opCodes.length; ++i) {
 			final String opCode = opCodes[i];
 			long opCodeLong;
-			try
-			{
+			try {
 				opCodeLong = Long.decode(opCode);
 			}
-			catch (Exception e)
-			{
-				return i > 0;
+			catch (Exception e) {
+				return i <= 0;
 			}
 			
-			if (opCodeLong < 0)
-			{
-				return false;
+			if (opCodeLong < 0) {
+				return true;
 			}
 			
-			if ((i == 0) && (opCodeLong > 255))
-			{
-				return false;
+			if ((i == 0) && (opCodeLong > 255)) {
+				return true;
 			}
-			else if ((i == 1) && (opCodeLong > 65535))
-			{
-				return false;
+			else if ((i == 1) && (opCodeLong > 65535)) {
+				return true;
 			}
-			else if ((i == 2) && (opCodeLong > 4294967295L))
-			{
-				return false;
+			else if ((i == 2) && (opCodeLong > 4294967295L)) {
+				return true;
 			}
 		}
 		
-		return true;
+		return false;
 	}
 	
-	private boolean validateFormat(String format)
-	{
-		for (int chIdx = 0; chIdx < format.length(); ++chIdx)
-		{
-			switch (format.charAt(chIdx))
-			{
-				case 'b':
-				case 'B':
-				case 'x':
-				case 'X':
-				{
-					// array
-					break;
-				}
-				case 'c':
-				case 'C':
-				{
-					// byte
-					break;
-				}
-				case 'h':
-				case 'H':
-				{
-					// word
-					break;
-				}
-				case 'd':
-				case 'D':
-				{
-					// dword
-					break;
-				}
-				case 'q':
-				case 'Q':
-				{
-					// qword
-					break;
-				}
-				case 'f':
-				case 'F':
-				{
-					// double
-					break;
-				}
-				case 's':
-				case 'S':
-				{
-					// string
-					break;
-				}
-				default:
-				{
-					return false;
-				}
+	private boolean hasInvalidFormat(String format) {
+		var upper = 1 << 5;
+		char[] validFormats = new char[] {
+			'b', 'c', 'd', 'f', 'h', 'q', 's', 'x'
+		};
+		for (int i = 0; i < format.length(); ++i) {
+			var digit = format.charAt(i);
+			if(Arrays.binarySearch(validFormats, (char) (digit | upper)) < 0) {
+				return true;
 			}
 		}
-		
-		return true;
+		return false;
 	}
 	
 	private boolean validateMethod(String method) {
@@ -195,17 +139,9 @@ public final class AdminPForge implements IAdminCommandHandler
 		showMainPage(activeChar);
 	}
 	
-	private void showSendUsage(Player activeChar, String[] opCodes, String format)
-	{
+	private void showSendUsage(Player activeChar) {
 		BuilderUtil.sendSysMessage(activeChar, "Usage: //forge_send sc|sb|cs opcode1[;opcode2[;opcode3]][ format value1 ... valueN] ");
-		if (opCodes == null)
-		{
-			showMainPage(activeChar);
-		}
-		else
-		{
-			showValuesPage(activeChar, opCodes, format);
-		}
+		showMainPage(activeChar);
 	}
 	
 	private void showMainPage(Player activeChar)
@@ -215,37 +151,37 @@ public final class AdminPForge implements IAdminCommandHandler
 	
 	private void showValuesPage(Player activeChar, String[] opCodes, String format)
 	{
-		String sendBypass;
+		StringBuilder sendBypass;
 		String valuesHtml = HtmCache.getInstance().getHtmForce(activeChar, "data/html/admin/pforge/values.htm");
 		if (opCodes.length == 3)
 		{
 			valuesHtml = valuesHtml.replace("%opformat%", "chd");
-			sendBypass = opCodes[0] + ";" + opCodes[1] + ";" + opCodes[2];
+			sendBypass = new StringBuilder(opCodes[0] + ";" + opCodes[1] + ";" + opCodes[2]);
 		}
 		else if (opCodes.length == 2)
 		{
 			valuesHtml = valuesHtml.replace("%opformat%", "ch");
-			sendBypass = opCodes[0] + ";" + opCodes[1];
+			sendBypass = new StringBuilder(opCodes[0] + ";" + opCodes[1]);
 		}
 		else
 		{
 			valuesHtml = valuesHtml.replace("%opformat%", "c");
-			sendBypass = opCodes[0];
+			sendBypass = new StringBuilder(opCodes[0]);
 		}
 		
-		valuesHtml = valuesHtml.replace("%opcodes%", sendBypass);
+		valuesHtml = valuesHtml.replace("%opcodes%", sendBypass.toString());
 		
-		String editorsHtml = "";
+		StringBuilder editorsHtml = new StringBuilder();
 		
 		if (format == null)
 		{
 			valuesHtml = valuesHtml.replace("%format%", "");
-			editorsHtml = "";
+			editorsHtml = new StringBuilder();
 		}
 		else
 		{
 			valuesHtml = valuesHtml.replace("%format%", format);
-			sendBypass += " " + format;
+			sendBypass.append(" ").append(format);
 			
 			final String editorTemplate = HtmCache.getInstance().getHtm(activeChar, "data/html/admin/pforge/inc/editor.htm");
 			
@@ -258,377 +194,250 @@ public final class AdminPForge implements IAdminCommandHandler
 				{
 					final char ch = format.charAt(chIdx);
 					singleCharSequence.setCharAt(0, ch);
-					editorsHtml += editorTemplate.replace("%format%", singleCharSequence).replace("%editor_index%", String.valueOf(chIdx));
-					sendBypass += " $v" + chIdx;
+					editorsHtml.append(editorTemplate.replace("%format%", singleCharSequence).replace("%editor_index%", String.valueOf(chIdx)));
+					sendBypass.append(" $v").append(chIdx);
 				}
 			}
 			else
 			{
-				editorsHtml = "";
+				editorsHtml = new StringBuilder();
 			}
 		}
 		
-		valuesHtml = valuesHtml.replace("%editors%", editorsHtml);
-		valuesHtml = valuesHtml.replace("%send_bypass%", sendBypass);
+		valuesHtml = valuesHtml.replace("%editors%", editorsHtml.toString());
+		valuesHtml = valuesHtml.replace("%send_bypass%", sendBypass.toString());
 		activeChar.sendPacket(new NpcHtmlMessage(0, 1, valuesHtml));
 	}
 	
 	@Override
-	public boolean useAdminCommand(String command, Player activeChar)
-	{
-		if (command.equals("admin_forge"))
-		{
+	public boolean useAdminCommand(String command, Player activeChar) {
+		if (command.equals("admin_forge")) {
 			showMainPage(activeChar);
 		}
-		else if (command.startsWith("admin_forge_values "))
-		{
-			try
-			{
-				final StringTokenizer st = new StringTokenizer(command);
-				st.nextToken(); // skip command token
-				
-				if (!st.hasMoreTokens())
-				{
-					showValuesUsage(activeChar);
-					return false;
-				}
-				
-				final String[] opCodes = getOpCodes(st);
-				if (!validateOpCodes(opCodes))
-				{
-					BuilderUtil.sendSysMessage(activeChar, "Invalid op codes!");
-					showValuesUsage(activeChar);
-					return false;
-				}
-				
-				String format = null;
-				if (st.hasMoreTokens())
-				{
-					format = st.nextToken();
-					if (!validateFormat(format))
-					{
-						BuilderUtil.sendSysMessage(activeChar, "Format invalid!");
-						showValuesUsage(activeChar);
-						return false;
-					}
-				}
-				
-				showValuesPage(activeChar, opCodes, format);
+		else if (command.startsWith("admin_forge_values ")) {
+			return showValues(command, activeChar);
+		}
+		else if (command.startsWith("admin_forge_send ")) {
+			return showSend(command, activeChar);
+		}
+		return true;
+	}
+
+	private boolean showSend(String command, Player activeChar) {
+		try {
+			final StringTokenizer st = new StringTokenizer(command);
+			st.nextToken(); // skip command token
+
+			if (!st.hasMoreTokens()) {
+				showSendUsage(activeChar);
+				return false;
 			}
-			catch (Exception e)
-			{
-				LOGGER.error(e.getMessage(), e);
+
+			final String method = st.nextToken();
+			if (!validateMethod(method)) {
+				BuilderUtil.sendSysMessage(activeChar, "Invalid method!");
+				showSendUsage(activeChar);
+				return false;
+			}
+
+			final String[] opCodes = st.nextToken().split(";");
+			if (isInvalidOpcodes(opCodes)) {
+				BuilderUtil.sendSysMessage(activeChar, "Invalid op codes!");
+				showSendUsage(activeChar);
+				return false;
+			}
+
+			String format = null;
+			if (st.hasMoreTokens()) {
+				format = st.nextToken();
+				if (hasInvalidFormat(format)) {
+					BuilderUtil.sendSysMessage(activeChar, "Format invalid!");
+					showSendUsage(activeChar);
+					return false;
+				}
+			}
+
+			if (!sendForgePacket(activeChar, st, method, opCodes, format)) {
+				return false;
+			}
+
+			showValuesPage(activeChar, opCodes, format);
+		}
+		catch (Exception e)
+		{
+			LOGGER.error(e.getMessage(), e);
+			showSendUsage(activeChar);
+			return false;
+		}
+		return true;
+	}
+
+	private boolean sendForgePacket(Player activeChar, StringTokenizer st, String method, String[] opCodes, String format) {
+		AdminForgePacket afp = new AdminForgePacket();
+		for (int i = 0; i < opCodes.length; ++i) {
+			char type = packetType(i);
+			afp.addPart((byte) type, opCodes[i]);
+		}
+
+		if (!addPacketContent(activeChar, st, format, afp)) {
+			return false;
+		}
+
+		if (method.equals("sc")) {
+			activeChar.sendPacket(afp);
+		} else if (method.equals("sb")) {
+			activeChar.broadcastPacket(afp);
+		}
+		return false;
+	}
+
+	private boolean addPacketContent(Player activeChar, StringTokenizer st, String format, AdminForgePacket afp) {
+		if(format == null) {
+			return true;
+		}
+
+		for (int i = 0; i < format.length(); ++i) {
+			if (!st.hasMoreTokens()) {
+				BuilderUtil.sendSysMessage(activeChar, "Not enough values!");
+				showSendUsage(activeChar);
+				return false;
+			}
+
+			String value = contentValue(activeChar, st);
+			afp.addPart((byte) format.charAt(i), value);
+		}
+		return true;
+	}
+
+	private String contentValue(Player activeChar, StringTokenizer st) {
+		WorldObject target = activeChar.getTarget();
+		String value = st.nextToken();
+		return switch (value) {
+			case "$oid" -> String.valueOf(activeChar.getObjectId());
+			case "$title" ->  activeChar.getTitle();
+			case "$name" ->  activeChar.getName();
+			case "$x" ->  String.valueOf(activeChar.getX());
+			case "$y" ->  String.valueOf(activeChar.getY());
+			case "$z" ->  String.valueOf(activeChar.getZ());
+			case "$heading" ->  String.valueOf(activeChar.getHeading());
+			case "$toid" ->  String.valueOf(activeChar.getTargetId());
+			case "$ttitle" ->  targetTitle(target);
+			case "$tname" ->  targetName(target);
+			case "$tx" ->  targetX(target);
+			case "$ty" ->  targetY(target);
+			case "$tz" ->  targetZ(target);
+			case "$theading" -> targetHeading(target);
+			default -> value;
+		};
+	}
+
+	private String targetHeading(WorldObject target) {
+		String value;
+		if (target != null) {
+			value = String.valueOf(target.getHeading());
+		} else {
+			value = "0";
+		}
+		return value;
+	}
+
+	private String targetZ(WorldObject target) {
+		String value;
+		if (target != null) {
+			value = String.valueOf(target.getZ());
+		} else {
+			value = "0";
+		}
+		return value;
+	}
+
+	private String targetY(WorldObject target) {
+		String value;
+		if (target != null) {
+			value = String.valueOf(target.getY());
+		} else {
+			value = "0";
+		}
+		return value;
+	}
+
+	private String targetX(WorldObject target) {
+		String value;
+		if (target != null) {
+			value = String.valueOf(target.getX());
+		} else {
+			value = "0";
+		}
+		return value;
+	}
+
+	private String targetName(WorldObject target) {
+		String value;
+		if (target != null) {
+			value = target.getName();
+		} else {
+			value = "";
+		}
+		return value;
+	}
+
+	private String targetTitle(WorldObject target) {
+		String value;
+		if (isCreature(target)) {
+			value = ((Creature) target).getTitle();
+		} else {
+			value = "";
+		}
+		return value;
+	}
+
+	private char packetType(int i) {
+		char type;
+		if (i == 0) {
+			type = 'c';
+		} else if (i == 1) {
+			type = 'h';
+		} else {
+			type = 'd';
+		}
+		return type;
+	}
+
+	private boolean showValues(String command, Player activeChar) {
+		try {
+			final StringTokenizer st = new StringTokenizer(command);
+			st.nextToken(); // skip command token
+
+			if (!st.hasMoreTokens()) {
 				showValuesUsage(activeChar);
 				return false;
 			}
-		}
-		else if (command.startsWith("admin_forge_send "))
-		{
-			try
-			{
-				final StringTokenizer st = new StringTokenizer(command);
-				st.nextToken(); // skip command token
-				
-				if (!st.hasMoreTokens())
-				{
-					showSendUsage(activeChar, null, null);
-					return false;
-				}
-				
-				final String method = st.nextToken();
-				if (!validateMethod(method))
-				{
-					BuilderUtil.sendSysMessage(activeChar, "Invalid method!");
-					showSendUsage(activeChar, null, null);
-					return false;
-				}
-				
-				final String[] opCodes = st.nextToken().split(";");
-				if (!validateOpCodes(opCodes))
-				{
-					BuilderUtil.sendSysMessage(activeChar, "Invalid op codes!");
-					showSendUsage(activeChar, null, null);
-					return false;
-				}
-				
-				String format = null;
-				if (st.hasMoreTokens())
-				{
-					format = st.nextToken();
-					if (!validateFormat(format))
-					{
-						BuilderUtil.sendSysMessage(activeChar, "Format invalid!");
-						showSendUsage(activeChar, null, null);
-						return false;
-					}
-				}
-				
-				AdminForgePacket afp = null;
-				ByteBuffer bb = null;
-				for (int i = 0; i < opCodes.length; ++i)
-				{
-					char type;
-					if (i == 0)
-					{
-						type = 'c';
-					}
-					else if (i == 1)
-					{
-						type = 'h';
-					}
-					else
-					{
-						type = 'd';
-					}
-					if (method.equals("sc") || method.equals("sb"))
-					{
-						if (afp == null)
-						{
-							afp = new AdminForgePacket();
-						}
-						afp.addPart((byte) type, opCodes[i]);
-					}
-					else
-					{
-						if (bb == null)
-						{
-							bb = ByteBuffer.allocate(32767);
-						}
-						write((byte) type, opCodes[i], bb);
-					}
-				}
-				
-				if (format != null)
-				{
-					for (int i = 0; i < format.length(); ++i)
-					{
-						if (!st.hasMoreTokens())
-						{
-							BuilderUtil.sendSysMessage(activeChar, "Not enough values!");
-							showSendUsage(activeChar, null, null);
-							return false;
-						}
-						
-						WorldObject target;
-						String value = st.nextToken();
-						switch (value)
-						{
-							case "$oid":
-							{
-								value = String.valueOf(activeChar.getObjectId());
-								break;
-							}
-							case "$title":
-							{
-								value = activeChar.getTitle();
-								break;
-							}
-							case "$name":
-							{
-								value = activeChar.getName();
-								break;
-							}
-							case "$x":
-							{
-								value = String.valueOf(activeChar.getX());
-								break;
-							}
-							case "$y":
-							{
-								value = String.valueOf(activeChar.getY());
-								break;
-							}
-							case "$z":
-							{
-								value = String.valueOf(activeChar.getZ());
-								break;
-							}
-							case "$heading":
-							{
-								value = String.valueOf(activeChar.getHeading());
-								break;
-							}
-							case "$toid":
-							{
-								value = String.valueOf(activeChar.getTargetId());
-								break;
-							}
-							case "$ttitle":
-							{
-								target = activeChar.getTarget();
-								if (isCreature(target))
-								{
-									value = ((Creature) target).getTitle();
-								}
-								else
-								{
-									value = "";
-								}
-								break;
-							}
-							case "$tname":
-							{
-								target = activeChar.getTarget();
-								if (target != null)
-								{
-									value = target.getName();
-								}
-								else
-								{
-									value = "";
-								}
-								break;
-							}
-							case "$tx":
-							{
-								target = activeChar.getTarget();
-								if (target != null)
-								{
-									value = String.valueOf(target.getX());
-								}
-								else
-								{
-									value = "0";
-								}
-								break;
-							}
-							case "$ty":
-							{
-								target = activeChar.getTarget();
-								if (target != null)
-								{
-									value = String.valueOf(target.getY());
-								}
-								else
-								{
-									value = "0";
-								}
-								break;
-							}
-							case "$tz":
-							{
-								target = activeChar.getTarget();
-								if (target != null)
-								{
-									value = String.valueOf(target.getZ());
-								}
-								else
-								{
-									value = "0";
-								}
-								break;
-							}
-							case "$theading":
-							{
-								target = activeChar.getTarget();
-								if (target != null)
-								{
-									value = String.valueOf(target.getHeading());
-								}
-								else
-								{
-									value = "0";
-								}
-								break;
-							}
-						}
-						
-						if (method.equals("sc") || method.equals("sb"))
-						{
-							if (afp != null)
-							{
-								afp.addPart((byte) format.charAt(i), value);
-							}
-						}
-						else
-						{
-							write((byte) format.charAt(i), value, bb);
-						}
-					}
-				}
-				
-				if (method.equals("sc"))
-				{
-					activeChar.sendPacket(afp);
-				}
-				else if (method.equals("sb"))
-				{
-					activeChar.broadcastPacket(afp);
-				}
-				else if (bb != null)
-				{
-					// TODO: Implement me!
-					// @formatter:off
-					/*bb.flip();
-					L2GameClientPacket p = (L2GameClientPacket) GameServer.gameServer.getL2GamePacketHandler().handlePacket(bb, activeChar.getClient());
-					if (p != null)
-					{
-						p.setBuffers(bb, activeChar.getClient(), new NioNetStringBuffer(2000));
-						if (p.read())
-						{
-							ThreadPool.executePacket(p);
-						}
-					}*/
-					// @formatter:on
-					throw new UnsupportedOperationException("Not implemented yet!");
-				}
-				
-				showValuesPage(activeChar, opCodes, format);
-			}
-			catch (Exception e)
-			{
-				LOGGER.error(e.getMessage(), e);
-				showSendUsage(activeChar, null, null);
+
+			final String[] opCodes = getOpCodes(st);
+			if (isInvalidOpcodes(opCodes)) {
+				BuilderUtil.sendSysMessage(activeChar, "Invalid op codes!");
+				showValuesUsage(activeChar);
 				return false;
 			}
-		}
-		
-		return true;
-	}
-	
-	private boolean write(byte b, String string, ByteBuffer buf)
-	{
-		if ((b == 'C') || (b == 'c'))
-		{
-			buf.put(Integer.decode(string).byteValue());
-			return true;
-		}
-		else if ((b == 'D') || (b == 'd'))
-		{
-			buf.putInt(Integer.decode(string));
-			return true;
-		}
-		else if ((b == 'H') || (b == 'h'))
-		{
-			buf.putShort(Integer.decode(string).shortValue());
-			return true;
-		}
-		else if ((b == 'F') || (b == 'f'))
-		{
-			buf.putDouble(Double.parseDouble(string));
-			return true;
-		}
-		else if ((b == 'S') || (b == 's'))
-		{
-			final int len = string.length();
-			for (int i = 0; i < len; i++)
-			{
-				buf.putChar(string.charAt(i));
+
+			String format = null;
+			if (st.hasMoreTokens()) {
+				format = st.nextToken();
+				if (hasInvalidFormat(format)) {
+					BuilderUtil.sendSysMessage(activeChar, "Format invalid!");
+					showValuesUsage(activeChar);
+					return false;
+				}
 			}
-			buf.putChar('\000');
-			return true;
+
+			showValuesPage(activeChar, opCodes, format);
 		}
-		else if ((b == 'B') || (b == 'b') || (b == 'X') || (b == 'x'))
-		{
-			buf.put(new BigInteger(string).toByteArray());
-			return true;
+		catch (Exception e) {
+			LOGGER.error(e.getMessage(), e);
+			showValuesUsage(activeChar);
+			return false;
 		}
-		else if ((b == 'Q') || (b == 'q'))
-		{
-			buf.putLong(Long.decode(string));
-			return true;
-		}
-		return false;
+		return true;
 	}
 	
 	@Override
