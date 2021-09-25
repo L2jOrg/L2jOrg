1dc09ef2e4280cee6e8b1346fed4ccae7a1c4f3c
Alisson Oliveira <joe.alisson@gmail.com>
2021-09-25 11:21:51 -0300

    Fix sonar's issues

--- Script/src/main/org.l2j.scripts/org/l2j/scripts/village/master/job/dwarf/blacksmith/second/DwarfBlacksmithChange2.java
+++ Script/src/main/org.l2j.scripts/org/l2j/scripts/village/master/job/dwarf/blacksmith/second/DwarfBlacksmithChange2.java
@@ -57,65 +57,54 @@ public final class DwarfBlacksmithChange2 extends AbstractNpcAI
 	}
 	
 	@Override
-	public String onAdvEvent(String event, Npc npc, Player player)
-	{
-		String htmltext = null;
-		switch (event)
-		{
-			case "30512-03.htm": // master_lv3_black006fa
-			case "30512-04.htm": // master_lv3_black007fa
-			case "30512-05.htm": // master_lv3_black007fat
-			{
-				htmltext = event;
-				break;
-			}
-			case "57":
-			{
-				htmltext = ClassChangeRequested(player, Integer.valueOf(event));
-				break;
-			}
-		}
-		return htmltext;
+	public String onAdvEvent(String event, Npc npc, Player player) {
+		return switch (event) {
+			case "30512-03.htm", "30512-04.htm", "30512-05.htm" -> event;
+			case "57" -> classChangeRequested(player, Integer.parseInt(event));
+			default -> null;
+		};
 	}
 	
-	private String ClassChangeRequested(Player player, int classId)
-	{
-		String htmltext = null;
-		if (player.isInCategory(CategoryType.THIRD_CLASS_GROUP))
-		{
-			htmltext = "30512-08.htm"; // fnYouAreThirdClass
+	private String classChangeRequested(Player player, int classId) {
+		String htmlText = null;
+		if (player.isInCategory(CategoryType.THIRD_CLASS_GROUP)) {
+			htmlText = "30512-08.htm";
+		} else if (classId == WARSMITH && player.getClassId() == ClassId.ARTISAN) {
+			htmlText = changeClassToWarSmith(player);
 		}
-		else if ((classId == WARSMITH) && (player.getClassId() == ClassId.ARTISAN))
+		return htmlText;
+	}
+
+	private String changeClassToWarSmith(Player player) {
+		String htmltext;
+		if (player.getLevel() < 40)
 		{
-			if (player.getLevel() < 40)
+			if (hasQuestItems(player, MARK_OF_GUILDSMAN, MARK_OF_PROSPERITY, MARK_OF_MAESTRO))
 			{
-				if (hasQuestItems(player, MARK_OF_GUILDSMAN, MARK_OF_PROSPERITY, MARK_OF_MAESTRO))
-				{
-					htmltext = "30512-09.htm"; // fnLowLevel11
-				}
-				else
-				{
-					htmltext = "30512-10.htm"; // fnLowLevelNoProof11
-				}
-			}
-			else if (hasQuestItems(player, MARK_OF_GUILDSMAN, MARK_OF_PROSPERITY, MARK_OF_MAESTRO))
-			{
-				takeItems(player, -1, MARK_OF_GUILDSMAN, MARK_OF_PROSPERITY, MARK_OF_MAESTRO);
-				player.setClassId(WARSMITH);
-				player.setBaseClass(WARSMITH);
-				// SystemMessage and cast skill is done by setClassId
-				player.broadcastUserInfo();
-				giveItems(player, SHADOW_ITEM_EXCHANGE_COUPON_C_GRADE, 15);
-				htmltext = "30512-11.htm"; // fnAfterClassChange11
+				htmltext = "30512-09.htm"; // fnLowLevel11
 			}
 			else
 			{
-				htmltext = "30512-12.htm"; // fnNoProof11
+				htmltext = "30512-10.htm"; // fnLowLevelNoProof11
 			}
+		}
+		else if (hasQuestItems(player, MARK_OF_GUILDSMAN, MARK_OF_PROSPERITY, MARK_OF_MAESTRO))
+		{
+			takeItems(player, -1, MARK_OF_GUILDSMAN, MARK_OF_PROSPERITY, MARK_OF_MAESTRO);
+			player.setClassId(WARSMITH);
+			player.setBaseClass(WARSMITH);
+			// SystemMessage and cast skill is done by setClassId
+			player.broadcastUserInfo();
+			giveItems(player, SHADOW_ITEM_EXCHANGE_COUPON_C_GRADE, 15);
+			htmltext = "30512-11.htm"; // fnAfterClassChange11
+		}
+		else
+		{
+			htmltext = "30512-12.htm"; // fnNoProof11
 		}
 		return htmltext;
 	}
-	
+
 	@Override
 	public String onTalk(Npc npc, Player player)
 	{
