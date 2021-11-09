ad869274acff3d0fcc95f02d8044575437cf6189
Alisson Oliveira <joe.alisson@gmail.com>
2021-11-09 07:54:21 -0300

    Fix Hp/Cp duplication

--- Script/src/main/org.l2j.scripts/org/l2j/scripts/handlers/effecthandlers/Cp.java
+++ Script/src/main/org.l2j.scripts/org/l2j/scripts/handlers/effecthandlers/Cp.java
@@ -18,72 +18,58 @@
  */
 package org.l2j.scripts.handlers.effecthandlers;
 
-import org.l2j.gameserver.engine.item.Item;
 import org.l2j.gameserver.engine.skill.api.Skill;
 import org.l2j.gameserver.engine.skill.api.SkillEffectFactory;
-import org.l2j.gameserver.enums.StatModifierType;
+import org.l2j.gameserver.model.DamageInfo;
 import org.l2j.gameserver.model.StatsSet;
 import org.l2j.gameserver.model.actor.Creature;
 import org.l2j.gameserver.model.effects.AbstractEffect;
 import org.l2j.gameserver.model.stats.Stat;
 import org.l2j.gameserver.network.SystemMessageId;
-import org.l2j.gameserver.network.serverpackets.SystemMessage;
-
-import static java.util.Objects.nonNull;
-import static org.l2j.gameserver.network.serverpackets.SystemMessage.getSystemMessage;
-import static org.l2j.gameserver.util.GameUtils.isDoor;
 
 /**
  * CP change effect. It is mostly used for potions and static damage.
  * @author Nik
  * @author JoeAlisson
  */
-public final class Cp extends AbstractEffect {
-
-    private final int power;
-    private final StatModifierType mode;
+public final class Cp extends AbstractVitalEffect {
 
     private Cp(StatsSet params) {
-        power = params.getInt("power", 0);
-        mode = params.getEnum("mode", StatModifierType.class, StatModifierType.DIFF);
+        super(params);
     }
 
     @Override
-    public boolean isInstant()
-    {
-        return true;
+    protected int maxVitalStat(Creature effected) {
+        return effected.getMaxCp();
     }
 
     @Override
-    public void instant(Creature effector, Creature effected, Skill skill, Item item) {
-        if (effected.isDead() || isDoor(effected) || effected.isHpBlocked()) {
-            return;
-        }
+    protected SystemMessageId healMessage() {
+        return SystemMessageId.S2_CP_HAS_BEEN_RESTORED_BY_C1;
+    }
 
-        double basicAmount = power;
-        if ( nonNull(item) && (item.isPotion() || item.isElixir())) {
-            basicAmount += effected.getStats().getValue(Stat.ADDITIONAL_POTION_CP, 0);
-        }
+    @Override
+    protected SystemMessageId selfHealingMessage() {
+        return SystemMessageId.S1_CP_HAS_BEEN_RESTORED;
+    }
 
-        double amount = switch (mode) {
-            case DIFF ->  Math.min(basicAmount, effected.getMaxRecoverableCp() - effected.getCurrentCp());
-            case PER ->  Math.min((effected.getMaxCp() * basicAmount) / 100.0, effected.getMaxRecoverableCp() - effected.getCurrentCp());
-        };
-
-        if (amount != 0) {
+    @Override
+    protected void heal(Creature effector, Creature effected, Skill skill, double amount) {
+        if(amount > 0) {
             effected.setCurrentCp(amount + effected.getCurrentCp(), false);
-            effected.broadcastStatusUpdate(effector);
+        } else {
+            effected.reduceCurrentHp(-amount, effector, skill, false, false, false, false, DamageInfo.DamageType.OTHER);
         }
+    }
 
-        if (amount > 0) {
-            SystemMessage sm;
-            if (nonNull(effector) && (effector != effected)) {
-                sm = getSystemMessage(SystemMessageId.S2_CP_HAS_BEEN_RESTORED_BY_C1).addString(effector.getName()).addInt((int) amount);
-            } else {
-                sm = getSystemMessage(SystemMessageId.S1_CP_HAS_BEEN_RESTORED);
-            }
-            effected.sendPacket(sm.addInt((int) amount));
-        }
+    @Override
+    protected double maxHealAllowed(Creature effected) {
+        return effected.getMaxRecoverableCp() - effected.getCurrentCp();
+    }
+
+    @Override
+    protected Stat additionalStat() {
+        return Stat.ADDITIONAL_POTION_CP;
     }
 
     public static class Factory implements SkillEffectFactory {
--- Script/src/main/org.l2j.scripts/org/l2j/scripts/handlers/effecthandlers/Hp.java
+++ Script/src/main/org.l2j.scripts/org/l2j/scripts/handlers/effecthandlers/Hp.java
@@ -18,77 +18,58 @@
  */
 package org.l2j.scripts.handlers.effecthandlers;
 
-import org.l2j.gameserver.engine.item.Item;
 import org.l2j.gameserver.engine.skill.api.Skill;
 import org.l2j.gameserver.engine.skill.api.SkillEffectFactory;
-import org.l2j.gameserver.enums.StatModifierType;
 import org.l2j.gameserver.model.DamageInfo;
 import org.l2j.gameserver.model.StatsSet;
 import org.l2j.gameserver.model.actor.Creature;
 import org.l2j.gameserver.model.effects.AbstractEffect;
 import org.l2j.gameserver.model.stats.Stat;
 import org.l2j.gameserver.network.SystemMessageId;
-import org.l2j.gameserver.network.serverpackets.SystemMessage;
-
-import static java.util.Objects.nonNull;
-import static org.l2j.gameserver.network.serverpackets.SystemMessage.getSystemMessage;
-import static org.l2j.gameserver.util.GameUtils.isDoor;
 
 /**
  * HP change effect. It is mostly used for potions and static damage.
  * @author Nik
  * @author JoeAlisson
  */
-public final class Hp extends AbstractEffect {
-	private final int power;
-	private final StatModifierType mode;
+public final class Hp extends AbstractVitalEffect {
 	
 	private Hp(StatsSet params) {
-		power = params.getInt("power", 0);
-		mode = params.getEnum("mode", StatModifierType.class, StatModifierType.DIFF);
+		super(params);
 	}
-	
-	@Override
-	public boolean isInstant()
-	{
-		return true;
-	}
-	
-	@Override
-	public void instant(Creature effector, Creature effected, Skill skill, Item item) {
-		if (effected.isDead() || isDoor(effected) || effected.isHpBlocked()) {
-			return;
-		}
 
-		double basicAmount = power;
-		if (nonNull(item) && (item.isPotion() || item.isElixir())) {
-			basicAmount += effected.getStats().getValue(Stat.ADDITIONAL_POTION_HP, 0);
-		}
-		
-		double amount = switch (mode) {
-			case DIFF -> Math.min(basicAmount, effected.getMaxRecoverableHp() - effected.getCurrentHp());
-			case PER -> Math.min((effected.getMaxHp() * basicAmount) / 100.0, effected.getMaxRecoverableHp() - effected.getCurrentHp());
-		};
-		
-		if (amount >= 0) {
-			if (amount != 0) {
-				effected.setCurrentHp(amount + effected.getCurrentHp(), false);
-				effected.broadcastStatusUpdate(effector);
-			}
-			
-			SystemMessage sm;
-			if (effector.getObjectId() != effected.getObjectId()) {
-				sm = getSystemMessage(SystemMessageId.S2_HP_HAS_BEEN_RESTORED_BY_C1).addString(effector.getName());
-			} else {
-				sm = getSystemMessage(SystemMessageId.S1_HP_HAS_BEEN_RESTORED);
-			}
-			sm.addInt((int) amount);
-			effected.sendPacket(sm);
+	@Override
+	protected int maxVitalStat(Creature effected) {
+		return effected.getMaxHp();
+	}
+
+	@Override
+	protected SystemMessageId healMessage() {
+		return SystemMessageId.S2_HP_HAS_BEEN_RESTORED_BY_C1;
+	}
+
+	@Override
+	protected SystemMessageId selfHealingMessage() {
+		return SystemMessageId.S1_HP_HAS_BEEN_RESTORED;
+	}
+
+	@Override
+	protected void heal(Creature effector, Creature effected, Skill skill, double amount) {
+		if(amount > 0) {
+			effected.setCurrentHp(amount + effected.getCurrentHp(), false);
 		} else {
-			final double damage = -amount;
-			effected.reduceCurrentHp(damage, effector, skill, false, false, false, false, DamageInfo.DamageType.OTHER);
-			effector.sendDamageMessage(effected, skill, (int) damage, 0, false, false);
+			effected.reduceCurrentHp(-amount, effector, skill, false, false, false, false, DamageInfo.DamageType.OTHER);
 		}
+	}
+
+	@Override
+	protected double maxHealAllowed(Creature effected) {
+		return effected.getMaxRecoverableHp() - effected.getCurrentHp();
+	}
+
+	@Override
+	protected Stat additionalStat() {
+		return Stat.ADDITIONAL_POTION_HP;
 	}
 
 	public static class Factory implements SkillEffectFactory {
